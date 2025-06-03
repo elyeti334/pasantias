da0fc46d50caf456d679bdf9c0cc65934c779ecc
elyeti334 <maximovillalba334@gmail.com>
2025-06-03 12:08:16 -0300

    Update clase1depython.py

--- clase1depython.py
+++ clase1depython.py
@@ -1,1 +1,10 @@
-
+color1 = "amarillo"
+color2 = "azul"
+color3 = "rojo"
+input(("el primer color")color1,color2,color3)
+if color1+color2 or color2+color1:
+ print("se formo el color verde")
+elif color1+color3 or color3+color1:
+ print("se formo el color naranja")
+elif color2+color3 or color3+color2:
+ print("se formo el color morado")
